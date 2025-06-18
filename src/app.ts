f6727cab1d84bbc19df643ed37651a56043b4a30
dongwoo <imkdw@kakao.com>
2025-06-18 18:19:22 +0900

    작업중

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import { App } from "@slack/bolt";
+import { config } from "dotenv";
+
+config();
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("⚡️ Bolt app is running!");
+})();
